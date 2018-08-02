298555603364ccd4a76204c4175df603b04613ea
Oxmel <oxm3l@hotmail.com>
2018-08-03 00:48:17 +0200

    partial rewrite of the connection procedure

--- src/wifi.py
+++ src/wifi.py
@@ -12,7 +12,6 @@ import diag
 FNULL = open(os.devnull, 'w')
 
 
-associate = "/sbin/iwconfig wlan0 essid 'FreeWifi' ap %s"
 dhcp_release = "/sbin/dhclient -r wlan0"
 dhcp_renew = "/sbin/dhclient wlan0"
 ap_signal = "iwconfig wlan0 | grep -Po 'level=\K\-[0-9]{2} [a-zA-Z]{3}'"
@@ -42,26 +41,30 @@ def call_cmd(cmd):
     return result
 
 
-
-
-def join_ap():
-    global cur_index
-    cur_ap = ap_list[cur_index]
-    logging.info("Connexion au point d'accès...")
-    call_cmd(associate %cur_ap)
-    # Give to the client some time to connect before checking the connection
-    time.sleep(5)
-
-    if diag.wifi_status() == 0:
-        logging.info("Connecté")
-        signal = check_cmd(ap_signal)
-        logging.info("BSSID: %s" %cur_ap)
-        logging.info("Signal: %s" %signal)
-        logging.debug("Demande d'attribution IP")
-        call_cmd(dhcp_renew)
+# We use ap=None to set an optional arg
+# So we can reuse the same method to connect to any AP with a given ESSID
+# Or to connect to a specific AP with a given ESSID and BSSID
+def join_ap(ap=None):
+    if ap:
+        associate = "/sbin/iwconfig wlan0 essid 'FreeWifi' ap %s" %ap
     else:
-        logging.warning("Echec de la connexion!")
+        associate = "/sbin/iwconfig wlan0 essid 'FreeWifi'"
 
+    try:
+        subprocess.check_call(associate, shell=True)
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
+
+# We use a blank essid to make the client disconnect from the current AP.
+# This command becomes useful when we want to start a scan because if the
+# client is connected to an AP, the scan result will only
+# show the AP he's currently connected to.
+def disconnect():
+    leave = "/sbin/iwconfig wlan0 essid ''"
+    try:
+        subprocess.check_call(leave, shell=True)
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
 
 
 def switch_ap():
@@ -80,4 +83,4 @@ def switch_ap():
     call_cmd(dhcp_release)
     logging.debug("Leaving -> %s" %cur_ap)
     logging.debug("Joining -> %s" %next_ap)
-    join_ap()
+    join_ap(next_ap)
