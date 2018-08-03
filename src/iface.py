82f4c693f4b0d4fb2460bd3053efadcf15eb4570
Oxmel <oxm3l@hotmail.com>
2018-08-03 16:03:42 +0200

    use a dedicated module for things related to interface management

---
+++ src/iface.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import logging
+import subprocess
+
+# Contains functions related to interface management
+# Those func can then be used elsewhere in the code when needed
+# For example, to make a dhcp_release, we can use :
+#import iface
+#iface.dhcp_action("release")
+
+
+def dhcp_action(action):
+    if action == "release":
+        cmd = "/sbin/dhclient -r wlan0"
+    elif action == "renew":
+        cmd = "/sbin/dhclient wlan0"
+
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
+
+
+def iface_action(action):
+    if action == "down":
+        cmd = "/sbin/ifconfig wlan0 down"
+    elif action == "up":
+        cmd = "/sbin/ifconfig wlan0 up"
+
+    try:
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
+
+
+
