aeb8fbe7422c7c642cc5b5619e1a0c5ece80b149
Oxmel <oxm3l@hotmail.com>
2018-12-26 18:23:53 +0100

    rewriting auth process (old method no longer works due to a server side update)

--- src/auth.py
+++ src/auth.py
@@ -7,33 +7,44 @@ import diag
 
 
 # Those credentials are needed in order to connect to the captive portal
-# The login is either a phone number or a mail address for orange hotspots
+# The username is either a phone number or a mail address for orange hotspots
 username = ''
 password = ''
 
-
-
-#logging.getLogger("urllib3").setLevel(logging.WARNING)
+# Humanize the script by using a 'normal' user-agent in the header
 user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0"
 
-auth_url = "https://id.orange.fr/auth_user/bin/auth_user.cgi"
-confirm_auth = "https://hautdebitmobile.orange.fr:8443/home/wassup"
-lost_url = "https://r.orange.fr/Oid_lost"
+# List of urls to use in order to log in the user
+init_cookie = "https://login.orange.fr"
+send_login = "https://login.orange.fr/front/login"
+send_pwd = "https://login.orange.fr/front/password"
+get_portal = "https://hautdebitmobile.orange.fr:8443/home"
+portal_connect = "https://hautdebitmobile.orange.fr:8443/home/wassup"
 
+# Pass the user-agent in the header of the request
+headers = { 'user-agent': user_agent }
+
+# Forge data before sending them to the server
+login_data = { 'login': username }
+pwd_data = { 'login': username, 'password': password }
+accept_cgu = {'isCgu': 'on', 'hidden_isCgu': '', 'doCheckCgu': '1'}
 
 def perform_auth():
-    headers = { 'user-agent' : user_agent }
+    # Let requests handle the cookies for us
     session = requests.Session()
-    cookies = {'co': '42'}
-    params = {'co': '42', 'tt': '', 'tp': '', 'sv': 'owa', 'dp': 'basic',
-            'losturl': lost_url, 'memorize_password': 'on', 'rl': confirm_auth,
-            'credential': username, 'password': password}
+
     try :
-        reqAuth = session.post(auth_url, cookies=cookies, data=params, headers=headers)
+        session.get(init_cookie)
+        session.post(send_login, json=login_data, headers=headers)
+        session.post(send_pwd, json=pwd_data, headers=headers)
+        session.get(get_portal, headers=headers)
+        session.get(portal_connect, params=accept_cgu, headers=headers)
+
         net_status = diag.network_check()
         if net_status == 0 :
             logging.info('Authentifié avec succès, accès internet confirmé')
         else :
             logging.error("Authentification ignorée par le serveur!")
+
     except requests.exceptions.RequestException as e:
         logging.error(e)
