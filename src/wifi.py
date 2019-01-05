ecac4274316ee8a16fd43573769602044e476f00
Oxmel <oxm3l@hotmail.com>
2019-01-05 18:34:10 +0100

    cleaning wifi module

--- src/wifi.py
+++ src/wifi.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import time
+
 import logging
 import subprocess
 import sys
 import os
+
 
 # Equivalent of /dev/null in bash
 FNULL = open(os.devnull, 'w')
@@ -34,30 +35,17 @@ def status():
 # To blacklist a specific ap : blacklist("mac_address")
 # To clear the blacklist : blacklist("clear")
 def blacklist(arg):
-
     cmd = "/sbin/wpa_cli blacklist %s" %arg
-
     try:
         subprocess.check_call(cmd, shell=True, stdout=FNULL)
     except subprocess.CalledProcessError as e:
         logging.error(e)
 
 
-# Tell wpa_supplicant to associate with any AP that shares a ssid
+# Tell wpa_supplicant to associate with any AP in the vicinity
+# An ESSID must be specified in '/etc/wpa_supplicant/wpa_supplicant.conf'
 def reassociate():
-
     cmd = "/sbin/wpa_cli reassociate"
-
-    try:
-        subprocess.check_call(cmd, shell=True, stdout=FNULL)
-    except subprocess.CalledProcessError as e:
-        logging.error(e)
-
-# Like reassociate but only takes effect if already disconnected
-def reconnect():
-
-    cmd = "/sbin/wpa_cli reconnect"
-
     try:
         subprocess.check_call(cmd, shell=True, stdout=FNULL)
     except subprocess.CalledProcessError as e:
